5da4a941bfab13ac4a42c0ad3c6949a54e9c3f1e
alex-sencabaugh <156012004+alex-sencabaugh@users.noreply.github.com>
2024-02-22 15:21:51 -0500

    hw changes

--- w6/script.js
+++ w6/script.js
@@ -1,55 +1,22 @@
+var hourPay = prompt("What is your hourly rate?")
+console.log(`hourPay: ${hourPay}`)
 
 
+var workHours = prompt("How many hours did you work (2 weeks)?")
+console.log(`workHours: ${workHours}`)
 
 
-//Regular string.
-var name = "Alex";
-var lastname = "Sencabaugh";
-//literal string example. 
-var message = `Hello welcome to the Javacript Lesson ${name}`;
-outputToConsole(message);
-//alert(message)
-var message2 = "hello" + " " + name + " " + lastname + " welcome to the javascript lesson";
-outputToConsole(message2)
-var addProblem = 12 + 4;
-outputToConsole(addProblem);
+var grossPay = workHours * hourPay
+console.log(`: ${grossPay}`)
 
 
-
-function outputToConsole(data) {
-    console.log(data)
-}
-
-//document.getElementById("#Header").innerHTML = message2;
-document.querySelector('body').style.backgroundColor = "#ffff33";
-
-var yourName = prompt("What is your name");
-document.querySelector('h1').innerHTML = `Hello ${yourName} thanks for clicking and entering your name!`;
+var taxes = grossPay * 0.2
+console.log(`: ${taxes}`)
 
 
-var dataPromptButton = document.getElementById("promptButton");
-dataPromptButton.addEventListener("click",dataPrompt)
+var netPay = grossPay - taxes
+console.log(`: ${netPay}`)
 
-
-function dataPrompt() {
-    alert(`my name is ${name}`);
-    var yourName = prompt("Please tell me your name.");
-    alert(`Hello${yourName}, Nice to meet you.`);
-
-    var yourLastName = prompt("Please tell me your last name.");
-    alert(`Great your full name is ${yourName} ${yourLastName}!`);
-
-    var firstNumber = prompt("Enter A number:")
-    var secondNumber = prompt("Enter another number:")
-
-    var result = Number(firstNumber) + Number(secondNumber);
-    alert(`the result is${result}`);
-
-    var documentBody = document.querySelector('body');
-
-    var outputData = document.createElement('p')
-
-    outputData.innerHTML = `your name is ${yourName} ${yourLastName}\n The result of your numbers is ${result} \n Thanks from ${name} ${lastname}`;
-    documentBody.appendChild(outputData);
-
-}
+alert(`Your gross pay is: ${grossPay.toFixed(2)}`)
+alert(`You paid: ${taxes.toFixed(2)} in taxes`)
+alert(`Your net pay is: ${netPay.toFixed(2)}`)
